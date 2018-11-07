0c0f3f96fbcbc3c50556ccfa182b080746769955
AlexWetzel <wetzela@live.com>
2018-11-06 21:09:31 -0500

    Added matching logic

--- logic.js
+++ logic.js
@@ -1,5 +1,37 @@
+function selectOne(select) {
+  console.log("foo")
+  firstSelect = select;
+
+  //...
+}
+
+function selectAnother(select) {
+  console.log("bar")
+
+  if(select === firstSelect) {
+    console.log("match!")
+  }
+  else {
+    console.log("no match")
+  }
+
+  firstSelect = "";
+}
+
+var firstSelect = "";
+var pairs = 2;
+var match = 0;
+
+
 
 
 $(".box").click(function() {
-  console.log("I'm working!");
+  var selection = $(this).attr("data-id");
+  
+  if(firstSelect === ""){
+    selectOne(selection);
+  }
+  else {
+    selectAnother(selection);
+  }
 })
