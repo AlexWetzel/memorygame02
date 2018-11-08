1f0a677101ef988110e335bf4e0e1da94128cd4f
AlexWetzel <wetzela@live.com>
2018-11-08 18:35:36 -0500

    added a timer that shows panels that don't match before flipping

--- logic.js
+++ logic.js
@@ -13,35 +13,47 @@ function selectAnother(select) {
   var first = firstSelect.attr("data-id");
   var second = select.attr("data-id");
   if(first === second) {
-    console.log("match!")
+    console.log("match!");
+    firstSelect = "";
   }
   else {
-    console.log("no match")
-    flipPanel(select);
-    flipPanel(firstSelect);
+    enableClicks = false;
+    console.log("no match");
+
+    window.setTimeout(function(){
+      flipPanel(select);
+      flipPanel(firstSelect);
+      firstSelect = "";
+      enableClicks = true;
+    }, 2000)
   }
 
-  firstSelect = "";
 }
 
 function flipPanel(panel) {
   panel.toggleClass("face_up face_down")
 }
 
+// Variable that holds the first selected element in a pair
 var firstSelect = "";
+// Number of pairs that need to matched to win
 var pairs = 2;
+// Number of matched panels
 var match = 0;
+// Used to prevent unintended behavior
+var enableClicks = true;
 
 
 $(document).on("click", ".face_down", function() {
-
-  var selection = $(this)
-  console.log(selection.attr("data-id"));
-  
-  if(firstSelect === ""){
-    selectOne(selection);
-  }
-  else {
-    selectAnother(selection);
+  if(enableClicks === true) {
+    var selection = $(this)
+    console.log(selection.attr("data-id"));
+    
+    if(firstSelect === ""){
+      selectOne(selection);
+    }
+    else {
+      selectAnother(selection);
+    }
   }
 })
